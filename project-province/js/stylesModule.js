f46172b0656db55e395c1df0677deed7b353f8c2
Anucha Kaewnil <anucha.k@cliniter.com>
2024-08-16 23:33:56 +0700

    update

--- project-province/js/stylesModule.js
+++ project-province/js/stylesModule.js
@@ -1,4 +1,4 @@
-import { itemsTour, itemsCarousel } from "./constants.js";
+import { itemsTour, itemsFood, itemsTrip, itemsCarousel } from "./constants.js";
 window.onscroll = function() {appear()};
 const appear = () => {
   const oneVhInPixels = window.innerHeight * 0.01;
@@ -20,13 +20,15 @@ const appear = () => {
 
 document.addEventListener("DOMContentLoaded", () => {
   tour();
+  food();
+  trip();
   carousel();
 });
 
 const tour = () => {
   const t = document.getElementById("tour")
   const show = itemsTour.map((i, index)=>
-    `<div class="card-container">
+    `<div class="card-container card-tour">
         <div class="card" data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-delay=${(index+1)*100}>
         <div class="card-header">${i.title}</div>
         <img class="card-img-top" src=${i.img} alt=${i.title}>
@@ -35,6 +37,35 @@ const tour = () => {
         </div>
       </div>
     </div>`
+  )
+  t.innerHTML = show.join("")
+}
+
+const food = () => {
+  const t = document.getElementById("food")
+  const show = itemsFood.map((i, index)=>
+    i.title != '' ?
+    `<div class="card-container card-food">
+        <div class="card" data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-delay=${(index+1)*100}>
+        <div class="card-header">${i.title}</div>
+        <img class="card-img-top" src=${i.img} alt=${i.title}>
+        <div class="card-body">
+          <p class="card-text">${i.info}</p>
+        </div>
+      </div>
+    </div>` : ''
+  )
+  t.innerHTML = show.join("")
+}
+
+const trip = () => {
+  const t = document.getElementById("trip")
+  const show = itemsTrip.map((i, index)=>
+    i.place != '' ?
+    `<li>
+      <a href=${i.url}><span>${i.day}:</span> ${i.title}<br><img src="${i.img}" alt=""></a>
+      <p class="mt-2">${i.info}</p>
+    </li>` : ''
   )
   t.innerHTML = show.join("")
 }
@@ -49,9 +80,10 @@ const carousel = () => {
   );
   const showID = itemsCarousel.map(
     (i, index) =>
+      i.title != '' ?
       `<li data-target="#carouselIndicators" data-slide-to="${index}" class=${
         index == 0 ? "active" : ""
-      }></li>`
+      }></li>` : ''
   )
   c.innerHTML = show.join("")
   id.innerHTML = showID.join("")
